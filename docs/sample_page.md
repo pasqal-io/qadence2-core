5c2d22c7b6e79dd26ab746317678a68885aec016
RolandMacDoland <9250798+RolandMacDoland@users.noreply.github.com>
2024-10-25 13:33:10 +0100

    [Infra] Add Qadence2-IR, Platforms and Expressions as dependencies and examples (#18)
    * add expressions and IR
    
    * update IR and Expressions
    
    * update ir and expressions
    
    * update expression
    
    * add platforms
    
    * add examples
    
    * add examples
    
    * add git submodule command on readme
    
    * add minor project changes
    
    * improve README
    
    * add first docs pages
    
    * add small version fixes
    
    * Update pyqtorch version.
    
    * Remove submodule as now dependencies.
    
    * Unpin dependencies.
    
    * Clean up obsolete file.
    
    ---------
    
    Co-authored-by: Kaonan Micadiei <kaonan.micadei@pasqal.com>
    Co-authored-by: Doomsk <eduardo.maschio@theqube.io>

--- docs/sample_page.md
+++
@@ -1,8 +0,0 @@
-This is just a sample notebook to showcase the rendering of Jupyter notebooks in the documentation.
-
-```python exec="on" source="material-block" session="main"
-from qadence2-core.main import main
-
-msg = main()
-print(msg)
-```
---
+++ examples/pulser_compiler.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import numpy as np
+from qadence2_expressions import RX, RY, add_qpu_directives, compile_to_model, parameter
+from qadence2_platforms.compiler import compile_to_backend
+
+a = parameter("a")
+expr = RX(1.57 * a)(0) * RY(0.707 * a**2)(0)
+print(f"expression: {str(expr)}")
+
+add_qpu_directives({"digital": True})
+model = compile_to_model(expr)
+print(f"model: {model}\n")
+
+f_params = {"a": np.array([1.0])}
+compiled_model = compile_to_backend(model, "fresnel1")
+res = compiled_model.sample(values=f_params, shots=10_000, on="emulator")
+print(f"sample result: {res}")
